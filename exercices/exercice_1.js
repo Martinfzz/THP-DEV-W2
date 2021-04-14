4cc330940ce3e4ff58f7e0db70967fffa604fa15
martinfzz <forget.l.martin@gmail.com>
2021-04-14 09:09:38 +0200

    Add exo1 and exo2

--- exercices/exercice_1.js
+++ exercices/exercice_1.js
@@ -1,4 +1,4 @@
-const data = require('./parseData');
+const data = require('../parseData');
 const k = Number(process.argv[3]);
 let comparisons = 0;
 
@@ -6,17 +6,15 @@ if (!k) {
   throw new Error('Please provide a valid number.')
 }
 
-const exercice1 = (numbers, resultK) => {
-  return numbers.some((value1, index1) => {
-      return numbers.some((value2, index2) => {
-          if (index2 === index1) {
-              return false;
-          }
-          comparisons += 1;
-          return ((value1 + value2) === resultK);
-      });
-  });
+const exercice1 = (arr, resultK) => {
+  for (let i = 1; i <= (arr.length - 1); i++){
+		for (let j = 0; j < (arr.length - i); j++){
+			if (arr[i-1] + arr[j+i] === resultK){
+			  return console.log(true);
+			}
+		}
+	}
+	return console.log(false);
 }
 
-const result = exercice1(data, k);
-console.log(result);
+exercice1(data, k);
---
+++ exercices/exercice_2.js
@@ -0,0 +1,22 @@
+const data = require('../parseData');
+
+const exercice2 = (arr) => {
+  let count = 1;
+  let n = arr.length;
+  let max = arr.indexOf(Math.max(...arr));
+  let newArr = []; 
+  if (Math.max(...arr) == arr[n-1]){
+    count = 1;
+    console.log(count);
+    return;
+  }
+  for (let i = max; i < (n); i++){
+    if (arr[i] > arr[i+1]){
+      newArr.push(arr[i]);
+    }
+  }
+  newArr.push(arr[n-1]);
+  count = newArr.length;
+	console.log(count);
+}
+exercice2(data);
